182b8ef02d3e9d21e1410a5a6bd0d5189b6e0698
Raph74300 <oundjian.r@gmail.com>
2024-06-14 13:03:34 +0200

    Initial Commit

---
+++ BitmapMsgEncoder/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BitmapMsgEncoder
+//
+//  Created by Raphaël Oundjian on 14/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
